687111d952730c9259ada1a51988bdd38cf8bd85
Mykola-Klymyshyn <klymyshyn33@gmail.com>
2022-11-22 20:57:56 +0200

    save-procces-menu-mobile

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const openMenuBtn = document.querySelector('.header__burger--open');
+    const closeMenuBtn = document.querySelector('.mobile-menu__close-btn');
   
     const toggleMenu = () => {
       const isMenuOpen =
